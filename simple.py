80765da304c620f2f3c2c13f4229ccedc16f5e95
F.I. Pelupessy <f.i.pelupessy@casema.nl>
2018-08-08 18:06:30 +0200

    small edits to example

--- simple.py
+++ simple.py
@@ -1,10 +1,15 @@
 from amuse.units import units
 
-from interface import HeatInterface, Heat
+from interface import Heat
   
 from matplotlib import pyplot  
+
+#~ import logging
+#~ logging.basicConfig(level=logging.DEBUG)
+#~ logging.getLogger("code").setLevel(logging.DEBUG)
+
   
-h=Heat(redirection='none')
+h=Heat(channel_type="mpi")#redirection='none',channel_type="sockets", debugger="xterm")
 
 pyplot.ion()
 f=pyplot.figure()
@@ -14,9 +19,13 @@ tnow=h.model_time
 tend=10
 dt=h.time_step
 
+print h.grid_0
+
+raw_input()
+
 while tnow<tend:
     h.evolve_model(tnow+dt)
-    tnow=h.get_current_time()
+    tnow=h.model_time
     print tnow
     
     f.clf()

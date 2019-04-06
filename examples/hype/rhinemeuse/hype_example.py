ccdc7df98b96fcc8d4290cb170d035b1ad09af80
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-04-06 14:31:21 +0200

    add var output

--- examples/hype/rhinemeuse/hype_example.py
+++ examples/hype/rhinemeuse/hype_example.py
@@ -6,6 +6,8 @@ h=Hype(redirection="none")
 
 h.parameters.ini_file="info.txt"
 
+print h.data_store_names()
+
 print h.grid_1
 
 print "time: ", h.model_time
@@ -14,6 +16,8 @@ print "evolve.."
 h.evolve_model(h.model_time + (400. | units.day))
 
 print "time: ", h.model_time
+
+print getattr(h.grid_1, "comp outflow olake")
 
 h.stop()
 
